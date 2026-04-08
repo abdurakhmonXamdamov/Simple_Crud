ee3d1d58bfa36a00ee6bdc819b873965f3efef53
abdurakhmonXamdamov <xamdamov.18.16@gmail.com>
2026-04-08 12:07:32 +0500

    views and urls are initialized

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
 
@@ -5,14 +6,18 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 
-    GENDER_CHOICES  = [
-        ('M', 'Male'),
-        ('F', 'Female')
-    ]
+    class GenderChoice(models.TextChoices):
+        M = 'Male',
+        F = 'Female'
+
     
-    phone = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    phone = models.CharField(max_length=15, unique=True, blank=True, default="")
     photo = models.ImageField(upload_to='photos/', null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
-    gender=models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    bio = models.TextField(blank=True, default="")
+    gender=models.CharField(
+            max_length=10, 
+            choices=GenderChoice.choices, 
+            blank=True,
+        )
     birth_date = models.DateField(null=True, blank=True)
 
---
+++ apps/accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import UserCreateAPIView
+
+urlpatterns = [
+    path('users/', UserCreateAPIView.as_view(), name='product-list'),
+]
